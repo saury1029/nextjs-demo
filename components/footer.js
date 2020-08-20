22500d6b4c05ea04d051d9b2e76e9b9ca6cfada8
zyan <645936468@qq.com>
2020-08-20 11:24:17 +0800

    nav/footer style change

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,30 @@ import Container from './container';
 const Footer = () => (
   <div className="text-xs bg-gray-200 text-gray-500">
     <Container>
-      <div className="text-center leading-10">
-        Next.js + Tailwind CSS + JSONPlaceholder
+      <div className="text-center leading-10 font-bold">
+        <a
+          href="https://nextjs.org/"
+          target="_blank"
+          className="text-gray-600 hover:text-primary"
+        >
+          Next.js
+        </a>{' '}
+        +{' '}
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          className="text-gray-600 hover:text-primary"
+        >
+          Tailwind CSS
+        </a>{' '}
+        +{' '}
+        <a
+          href="http://jsonplaceholder.typicode.com/"
+          target="_blank"
+          className="text-gray-600 hover:text-primary"
+        >
+          JSONPlaceholder
+        </a>
       </div>
     </Container>
   </div>
--- components/nav.js
+++ components/nav.js
@@ -1,37 +1,35 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Container from './container';
 
-const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
-];
-
 export default function Nav() {
+  const router = useRouter();
+  console.log(router);
+  const links = ['posts', 'users', 'photos'];
+
   return (
     <nav className="border-b">
       <Container>
         <div className="flex flex-col md:flex-row justify-between items-center py-4">
           <Link href="/">
-            <a className="text-2xl text-gray-700 font-bold no-underline">
+            <a className="text-3xl text-gray-700 font-bold no-underline">
               Next.js Demo
             </a>
           </Link>
           <div className="flex space-x-8 mt-4 md:mt-0">
-            <Link href="/posts">
-              <a className="text-gray-700 hover:text-primary hover:underline">
-                POSTS
-              </a>
-            </Link>
-            <Link href="/users">
-              <a className="text-gray-700 hover:text-primary hover:underline">
-                USERS
-              </a>
-            </Link>
-            <Link href="/photos">
-              <a className="text-gray-700 hover:text-primary hover:underline">
-                PHOTOS
-              </a>
-            </Link>
+            {links.map((link) => (
+              <Link href={`/${link}`}>
+                <a
+                  className={`hover:text-primary ${
+                    router.pathname === `/${link}`
+                      ? 'text-primary'
+                      : 'text-gray-700'
+                  }`}
+                >
+                  {link.toUpperCase()}
+                </a>
+              </Link>
+            ))}
           </div>
         </div>
       </Container>
